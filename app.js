1af28a653f9354757ed392d52657c0942c36ffd4
HXY <1969574105@qq.com>
2022-07-15 09:38:57 +0800

    👑：添加cors配置cors跨域

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const bodyParser = require('body-parser');
 const fileUpload = require('express-fileupload');
 const config = require('./config');
 const mongoose = require("mongoose")
+const cors = require('cors')
+
 const mongodbUrl = config.MONGODB_URL;
 mongoose
     .connect(mongodbUrl)
@@ -21,16 +23,8 @@ app.use(bodyParser.json())
 app.use(function (err, req, res, next) {
     res.status(500).send({ message: "服务器错误" })
 });
-// 配置跨域请求中间件(服务端允许跨域请求)
-var allowCors = function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*"); // 设置允许来自哪里的跨域请求访问（req.headers.origin为当前访问来源的域名与端口）
-    res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS"); // 设置允许接收的请求类型
-    res.header("Access-Control-Allow-Headers", "Content-Type,request-origin"); // 设置请求头中允许携带的参数
-    res.header("Access-Control-Allow-Credentials", "true"); // 允许客户端携带证书式访问。保持跨域请求中的Cookie。注意：此处设true时，Access-Control-Allow-Origin的值不能为 '*'
-    res.header("Access-control-max-age", 1000); // 设置请求通过预检后多少时间内不再检验，减少预请求发送次数
-    next();
-};
-app.use(allowCors); // 使用跨域中间件
+
+app.use("*", cors())
 
 app.use('/public/', express.static('./public/'))
 app.use('/api/admins', adminRoute);
