f8ef9ea83c23418267293b7a12219d8233f93082
HXY <1969574105@qq.com>
2022-07-18 13:12:12 +0800

    👑：添加数据库连接认证

--- config.js
+++ config.js
@@ -3,7 +3,7 @@
 
 module.exports = {
   PORT: process.env.PORT || 8081,
-  MONGODB_URL: process.env.MONGODB_URL || 'mongodb://101.43.146.27/csms',
+  MONGODB_URL: process.env.MONGODB_URL || 'mongodb://admin:@Hxy020414@101.43.146.27/csms',
   JWT_SECRET: process.env.JWT_SECRET || 'jwtsecret',
   HXY_ADMIN_NAME : "黄显瑜"
 };
