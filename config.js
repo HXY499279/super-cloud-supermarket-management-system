4b2b4c97db28e357127f7b6ff55474db7529c417
HXY <1969574105@qq.com>
2022-09-12 18:01:45 +0800

    👑：修复线上bug—端口号8080占用问题

--- config.js
+++ config.js
@@ -2,7 +2,7 @@
 // dotenv.config();
 
 module.exports = {
-  PORT: process.env.PORT || 8080,
+  PORT: process.env.PORT || 8081,
   MONGODB_URL: process.env.MONGODB_URL || 'mongodb://hxy:hxy020414@101.43.146.27/csms?authSource=admin',
   JWT_SECRET: process.env.JWT_SECRET || 'jwtsecret',
   HXY_ADMIN_NAME : "黄显瑜"
